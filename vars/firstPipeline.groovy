f6e2da965f24aecc6f4b6d0a665ba18007403867
KISHORESAMALA1725 <kishore.9686@gmail.com>
2024-11-14 13:24:51 +0530

    commit1

---
+++ vars/firstPipeline.groovy
@@ -0,0 +1,26 @@
+import com.i27academy.builds.Calculator
+
+def call(map pipelineparam) {
+    Calculator calculator = new Calculator()
+    pipeline {
+        agent {
+            label 'java-slave'
+        }
+        environment {
+            APPLICATION_NAME = "${pipelineparams.appName}"
+        }
+        stages {
+            stage ('this is SHARED_LIB_EXAMPLE') {
+                steps {
+                    script {
+                        echo "calling calculator method from src folder"
+                        echo " * * * * *  PRINTING SUM OF VALUES * * * * * "
+                        println calculator(2,3)
+                    }
+                }
+            }
+            
+        }
+
+    }
+}
