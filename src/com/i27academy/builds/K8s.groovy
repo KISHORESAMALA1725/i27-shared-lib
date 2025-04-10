eac3e16c251e7610c2db56cd07ec8b18781d4c43
kishore <kishore.9686@gmail.com>
2025-04-10 08:54:39 +0530

    added K8s.groovy

---
+++ src/com/i27academy/builds/K8s.groovy
@@ -0,0 +1,20 @@
+package com.i27academy.K8s;
+
+class K8s {
+    def jenkins
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    // Method TO Authenticate to K8S Cluster
+
+    def auth_login(clusterName, zone, projectID) {
+        jenkins.sh """
+        echo " ***** creating k8s authentication login menthod ***** " 
+        gcloud compute instance list
+        echo " ***** create config file for environment ***** "
+        gcloud container clusters get-credentials ${clustername}--zone ${zone} --project ${projectID}
+        kubectl get nodes
+        """
+    }
+}
