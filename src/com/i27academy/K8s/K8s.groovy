bf6978205a4c5d494eb4f6a8ab900257440338b0
KISHORESAMALA1725 <kishore.9686@gmail.com>
2025-04-10 21:34:35 +0530

    Update K8s.groovy

--- src/com/i27academy/K8s/K8s.groovy
+++ src/com/i27academy/K8s/K8s.groovy
@@ -11,7 +11,7 @@ class K8s {
     def auth_login(clusterName, zone, projectID) {
         jenkins.sh """
         echo " ***** creating k8s authentication login menthod ***** " 
-        gcloud compute instance list
+        gcloud compute instances list
         echo " ***** create config file for environment ***** "
         gcloud container clusters get-credentials $clusterName --zone $zone --project $projectID
         kubectl get nodes
