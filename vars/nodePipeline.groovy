a31bd8f14ba6abba7d200eb7b588f909a910e943
kishore <kishore.9686@gmail.com>
2025-04-16 11:41:55 +0530

    fcdsa

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -1,197 +1,258 @@
-import com.i27academy.builds.Node;
+import com.i27academy.builds.Docker;
+import com.i27academy.K8s.K8s;
 
-pipeline {
+def call (Map pipelineParams) {
+    K8s K8s = new K8s(this)
+    pipeline {
     agent {
         label 'k8s-slave'
     }
-
     parameters {
-        choice(name: 'buildOnly', choices: 'no\nyes', description: 'This will run the build for Node.js app')
-        choice(name: 'scanOnly',  choices: 'no\nyes', description: 'This will run the SonarQube scan')
-        choice(name: 'dockerPush',choices: 'no\nyes', description: 'This will trigger build, SonarQube scan, image build & push to repo')
-        choice(name: 'deploytoDev', choices: 'no\nyes', description: 'This will deploy to dev')
-        choice(name: 'deploytoTest', choices: 'no\nyes', description: 'This will deploy to test')
-        choice(name: 'deploytoStage', choices: 'no\nyes', description: 'This will deploy to stage')
-        choice(name: 'deploytoProd', choices: 'no\nyes', description: 'This will deploy to prod')
+        // choice(name: 'buildOnly', choices: 'no\nyes', description: 'this will run maven build')
+        // choice(name: 'scanOnly',  choices: 'no\nyes', description: 'this will run sonarscans')
+        choice(name: 'dockerPush',choices: 'no\nyes', description: 'this will trigger build,sonarscan,image is build & pushed to repo')
+        choice(name: 'deploytoDev', choices: 'no\nyes', description: 'this will deploy to dev')
+        choice(name: 'deploytoTest', choices: 'no\nyes', description: 'this will deploy to test')
+        choice(name: 'deploytoStage', choices: 'no\nyes', description: 'this will deploy to stage')
+        choice(name: 'deploytoProd', choices: 'no\nyes', description: 'this will deploy to prod')
     }
 
-    tools {
-        nodejs 'NodeJS-14'  // Ensure NodeJS-14 is configured in Jenkins tools
-    }
-
+    // tools {
+    //     maven 'maven-3.8.8'
+    //     jdk 'jdk-17'
+    // }
+    
     environment {
         APPLICATION_NAME = "${pipelineParams.appName}"
-        DEV_HOST_PORT = "${pipelineParams.devHostPort}"
-        TEST_HOST_PORT = "${pipelineParams.testHostPort}"
-        STAGE_HOST_PORT = "${pipelineParams.stageHostPort}"
-        PROD_HOST_PORT = "${pipelineParams.prodHostPort}"
+        // DEV_HOST_PORT = "${pipelineParams.devHostPort}"
+        // TST_HOST_PORT = "${pipelineParams.tstHostPort}"
+        // STG_HOST_PORT = "${pipelineParams.stgHostPort}"
+        // PROD_HOST_PORT = "${pipelineParams.prodHostPort}"
         CONT_PORT = "${pipelineParams.contPort}"
+        POM_VERSION = readMavenPom().getVersion() 
+        POM_PACKAGING = readMavenPom().getPackaging()
         DOCKER_HUB = "docker.io/kishoresamala84"
-        DOCKER_CREDS = credentials("kishoresamala84_docker_creds")
+        DOCKER_CREDS = credentials('kishoresamala84_docker_creds')
+
+        DEV_CLUSTER_NAME = "i27-cluster"
+        DEV_CLUSTER_ZONE = "us-central1-c"
+        DEV_PROJECT_ID = "saharssh-456212"
+
+        K8S_DEV_FILE = "k8s_dev.yaml"
+        K8S_TEST_FILE = "k8s_test.yaml"
+        K8S_STAGE_FILE = "k8s_stage.yaml"
+        K8S_PROD_FILE = "k8s_prod.yaml"
+
+        DEV_NAMESPACE = "cart-dev-ns"
+        TEST_NAMESPACE = "cart-test-ns"
+        STAGE_NAMESPACE = "cart-stage-ns"
+        PROD_NAMESPACE = "prod-stage-ns"
     }
 
     stages {
-        stage('Checkout') {
-            steps {
-                checkout scm  // Checkout the source code from the repository
-            }
-        }
+        // stage (' ***** BUILD STAGE ***** ') {
+        //     when {
+        //         expression {
+        //             params.buildOnly == 'yes'
+        //         }
+        //     }
+        //     steps {
+        //         script {
+        //             docker.buildApp("${env.APPLICATION_NAME}")
+        //         }
+        //     }
+        // }
 
-        stage('Install Dependencies') {
-            when {
-                expression { params.buildOnly == 'yes' }
-            }
+        // stage (' ***** SONARQUBE STAGE ***** ') {
+        //     when {
+        //         anyOf {
+        //         expression {
+        //             params.buildOnly == 'yes'
+        //             params.scanOnly == 'yes'                    
+        //             params.dockerPush == 'yes'
+        //         }
+        //     }
+
+        //     }
+        //     steps {
+        //         echo "***** sonar stage implementing *****"                
+        //         withSonarQubeEnv('sonarqube') {
+        //                 sh """
+        //                     mvn clean verify sonar:sonar \
+        //                     -Dsonar.projectKey=i27-eureka-05 \
+        //                     -Dsonar.host.url=http://34.85.201.20:9000/ \
+        //                     -Dsonar.login=sqa_3ed9d5a5f6a874816c2f1c19b9c7674747b81b21
+        //                 """
+        //         }
+        //         timeout(time: 2, unit: 'MINUTES') {
+        //             waitForQualityGate abortPipeline: true
+        //         }
+        //     }
+        // }
+
+        stage ('BUILD-FORMAT') {
             steps {
                 script {
-                    echo "Installing Node.js dependencies..."
-                    installDependencies()  // Call the method to install dependencies
+                    // Existing : i27-eureka-0.0.1-SNAPSHOT.jar
+                    // Destination: i27-eureka-buildnumber-branchname.packagin
+                    sh """
+                    echo "Testing source jar-source: i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING}"
+                    echo "Tesing destination Jar: i27-${env.APPLICATION_NAME}-${currentBuild.number}-${BRANCH_NAME}.${env.POM_PACKAGING}"
+                    """ 
                 }
             }
         }
 
-        stage('Run Tests') {
+        stage (' ***** Docker-Build-Push ***** ') {
             when {
-                expression { params.buildOnly == 'yes' }
+                expression {
+                    params.dockerPush == 'yes'
+                }
             }
             steps {
                 script {
-                    echo "Running tests..."
-                    runTests()  // Call the method to run tests
+                    dockerBuildAndPush().call()
+            }
+        }
+
+      }
+
+        stage (' ***** Deploy to dev ***** ') {
+            when {
+                expression {
+                    params.deploytoDev == 'yes'
+                }
+            }
+            steps {
+                script {
+                    def docker_image = "${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+                    K8s.auth_login("${env.DEV_CLUSTER_NAME}","${env.DEV_CLUSTER_ZONE}","${env.DEV_PROJECT_ID}")
+                    imageValidation().call()
+                    K8s.K8sdeploy("${env.K8S_DEV_FILE}", docker_image, "${env.DEV_NAMESPACE}")
+                    // dockerDeploy('dev', "${env.DEV_HOST_PORT}", "${env.CONT_PORT}").call()
+                }
+
+            }
+        }
+
+        stage (' ***** Deploy to Test ***** ') {
+            when {
+                expression {
+                    params.deploytoTest == 'yes'
+                }
+            }
+            steps {
+                script {
+                    imageValidation().call()
+                    dockerDeploy('tst', "${env.TST_HOST_PORT}", "${env.CONT_PORT}").call()
                 }
             }
         }
 
-        stage('SonarQube Analysis') {
+        stage (' ***** Deploy to Stage ***** ') {
             when {
+                allOf{
+                    anyOf{
+                        expression {
+                            params.deploytoStage == 'yes'
+                        }
+                    }
+                    anyOf{ 
+                            branch 'release/*'
+                            tag pattern: "v\\d{1,2}\\.\\d{1,2}\\.\\d{1,2\\}", comparator: "REGEXP"
+ 
+                    }
+                }
+            }
+
+            steps {
+                script {
+                    imageValidation().call()
+                    dockerDeploy('stg', "${env.STG_HOST_PORT}", "${env.CONT_PORT}").call()
+                }
+            }
+        }
+
+        stage (' ***** Deploy to PROD ***** ') {
+            when {
+                allOf {
+                    anyOf{
+                      expression {
+                         params.deploytoProd == 'yes'
+                    }
+                }
                 anyOf {
-                    expression { params.buildOnly == 'yes' }
-                    expression { params.scanOnly == 'yes' }
-                    expression { params.dockerPush == 'yes' }
+                    branch 'release/*'
+                    tag pattern: "v.\\d{1,2\\}.\\d{1,2}.\\d{1,2}\\"
                 }
-            }
-            steps {
-                withSonarQubeEnv('sonarqube') {
-                    echo "Running SonarQube scan..."
-                    runSonarQubeAnalysis()  // Call the method to run SonarQube analysis
-                }
-                timeout(time: 2, unit: 'MINUTES') {
-                    waitForQualityGate abortPipeline: true
-                }
-            }
-        }
 
-        stage('Docker Build and Push') {
-            when {
-                expression { params.dockerPush == 'yes' }
-            }
-            steps {
-                script {
-                    echo "Building Docker image..."
-                    dockerBuildAndPush()  // Call the method to build and push Docker image
-                }
-            }
-        }
-
-        stage('Deploy to Dev') {
-            when {
-                expression { params.deploytoDev == 'yes' }
-            }
-            steps {
-                script {
-                    echo "Deploying to Dev environment..."
-                    deployToEnv('dev', DEV_HOST_PORT)  // Call the method to deploy to dev
-                }
-            }
-        }
-
-        stage('Deploy to Test') {
-            when {
-                expression { params.deploytoTest == 'yes' }
-            }
-            steps {
-                script {
-                    echo "Deploying to Test environment..."
-                    deployToEnv('test', TEST_HOST_PORT)  // Call the method to deploy to test
-                }
-            }
-        }
-
-        stage('Deploy to Stage') {
-            when {
-                allOf {
-                    expression { params.deploytoStage == 'yes' }
-                    anyOf {
-                        branch 'release/*'
-                        tag pattern: "v\\d{1,2}\\.\\d{1,2}\\.\\d{1,2}", comparator: "REGEXP"
-                    }
                 }
             }
             steps {
-                script {
-                    echo "Deploying to Stage environment..."
-                    deployToEnv('stage', STAGE_HOST_PORT)  // Call the method to deploy to stage
+                timeout(time: 300, unit: 'SECONDS') {
+                    input message: "Deploying to ${env.APPLICATION_NAME} to Production" , ok: 'yes', submitter: 'sivasre,i27academy'
                 }
-            }
-        }
-
-        stage('Deploy to Prod') {
-            when {
-                allOf {
-                    expression { params.deploytoProd == 'yes' }
-                    anyOf {
-                        branch 'release/*'
-                        tag pattern: "v\\d{1,2}\\.\\d{1,2}\\.\\d{1,2}", comparator: "REGEXP"
-                    }
-                }
-            }
-            steps {
-                script {
-                    echo "Deploying to Prod environment..."
-                    deployToEnv('prod', PROD_HOST_PORT)  // Call the method to deploy to prod
+                    script {
+                    dockerDeploy('prd', "${env.PROD_HOST_PORT}", "${env.contPort}").call()
                 }
             }
         }
     }
+}
+    
+}
 
-    post {
-        always {
-            echo 'Pipeline completed!'
-        }
-        success {
-            echo 'Build succeeded!'
-        }
-        failure {
-            echo 'Build failed.'
-        }
+
+
+def buildApp() {
+    return {
+            echo "*****Building the Application *****"
+            sh "mvn clean package -DskipTest=true"
+            archiveArtifacts 'target/*.jar'        
     }
 }
 
-// Move method definitions outside the pipeline block
-def installDependencies() {
-    echo "Installing dependencies using npm..."
-    sh 'npm install'  // Install Node.js dependencies
-}
+def imageValidation() {
+    return {
+        println(' *****Attempting to pulling the docker image***** ')
+        try {
+            sh "docker pull ${env.DOCKER_HUB}/${env.APPLICATON_NAME}:${GIT_COMMIT}"
+            println('*****Docker Image Pulled successfully')
+        }
+        catch (Exception e){
+            echo "OOOPPSSS!!! Docker Image with this tag is not found, so image is building now"
+            buildApp().call()
+            dockerBuildAndPush().call()
+        }
 
-def runTests() {
-    echo "Running tests using npm..."
-    sh 'npm test'  // Run tests (ensure you have a test script in package.json)
-}
-
-def runSonarQubeAnalysis() {
-    echo "Running SonarQube analysis..."
-    sh 'npm run sonar'  // Ensure you have a sonar script defined in your package.json
+    }
 }
 
 def dockerBuildAndPush() {
-    echo "Building Docker image..."
-    sh 'docker build -t ${DOCKER_HUB}/${APPLICATION_NAME}:${GIT_COMMIT} .'  // Build Docker image
-    echo "Logging into Docker Hub..."
-    sh 'docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}'  // Login to Docker Hub
-    echo "Pushing Docker image to Docker Hub..."
-    sh 'docker push ${DOCKER_HUB}/${APPLICATION_NAME}:${GIT_COMMIT}'  // Push Docker image to Docker Hub
+    return {
+        echo "****** Building Doker image *******"                    
+        sh "cp ${WORKSPACE}/target/i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd"
+        sh "docker build --no-cache --build-arg JAR_SOURCE=i27-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd "
+        sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
+        echo "****** Building Doker image *******" 
+        sh "docker push ${DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"                    
+       }        
 }
 
-def deployToEnv(env, hostPort) {
-    echo "Deploying ${APPLICATION_NAME} to ${env} environment..."
-    sh "docker run -d -p ${hostPort}:${CONT_PORT} --name ${APPLICATION_NAME}-${env} ${DOCKER_HUB}/${APPLICATION_NAME}:${GIT_COMMIT}"  // Deploy to environment
+def dockerDeploy(envDeploy, hostPort, contPort) {
+    return {
+                echo " ***** deploy to $envDeploy env ***** "
+                withCredentials([usernamePassword(credentialsId: 'john_docker_vm_passwd', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        script {    
+                            try {
+                                sh "sshpass -p '$PASSWORD' -v ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker stop ${env.APPLICATION_NAME}-$envDeploy \""
+                                sh "sshpass -p '$PASSWORD' -v ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker rm ${env.APPLICATION_NAME}-$envDeploy \""
+                            }
+                            catch(err){
+                                echo "Error Caught: $err"                      
+                            }  
+                            sh "sshpass -p '$PASSWORD' -v ssh -o StrictHostKeyChecking=no '$USERNAME'@$dev_ip \"docker container run -dit -p $hostPort:$contPort --name ${env.APPLICATION_NAME}-$envDeploy ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}\""                    
+                        }
+                   }
+    }
 }
